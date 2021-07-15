4306091608884758e0e4f9980f8f22bc4b375b27
dawndavidson <84029532+dawndavidson@users.noreply.github.com>
2021-07-15 07:49:05 -0500

    'studio'

---
+++ org.launchcode.java.studios.areaofacircle/src/com/company/Main.java
@@ -0,0 +1,22 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please enter the circle radius: ");
+        String userInput = input.nextLine();
+
+        double radius = Double.parseDouble(userInput);
+
+        System.out.println("The area of a circle with radius" + radius + "is" + calculateCircleArea(radius));
+
+    }
+
+    public static double calculateCircleArea(double userRadius){
+        return Math.PI * userRadius * userRadius;
+    }
+}
